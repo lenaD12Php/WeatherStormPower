ab4874c9dc1a5e7767d23226fbc383f017fbb2ce
Lena Damisi <Lena.damisi12@gmail.com>
2024-10-17 15:41:37 +0300

    Created a class called BotConfiguration that will handle the JsonFile

---
+++ WeatherStormPower/BotConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStormPower
+{
+    internal class BotConfiguration
+    {
+    }
+}
