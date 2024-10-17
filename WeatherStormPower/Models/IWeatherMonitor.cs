5ded2a929102a960d9e1d25e21658ddea704c92c
Lena Damisi <Lena.damisi12@gmail.com>
2024-10-17 15:39:44 +0300

    Added a IWeatherMonitor and WeatherMonitor containing registerBots , UnregisterBots and ModifyBots

---
+++ WeatherStormPower/Models/IWeatherMonitor.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherStormPower.Models;
+
+public interface IWeatherMonitor
+{
+    void RegisterBot(IWeatherBot bot);
+    void UnregisterBot(IWeatherBot bot);
+    void NotifyBots(WeatherData data);
+}
---
+++ WeatherStormPower/Models/WeatherMonitor.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherStormPower.Models;
+
+public class WeatherMonitor :IWeatherMonitor
+{
+    private List<IWeatherBot> _bots = new List<IWeatherBot>();
+
+    public void RegisterBot(IWeatherBot bot)
+    {
+        _bots.Add(bot);
+    }
+
+    public void UnregisterBot(IWeatherBot bot)
+    {
+        _bots.Remove(bot);
+    }
+
+    public void NotifyBots(WeatherData data)
+    {
+        foreach (var bot in _bots)
+            bot.UpdateWeather(data);
+    }
+}
